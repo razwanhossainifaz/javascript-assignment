74e66d2018522f9eb9a2ccde177f447a3f210ec9
freelancerifaz <mrifaz369@gmail.com>
2020-07-17 01:56:12 +0600

    added wood and brick calculator

--- assignment.js
+++ assignment.js
@@ -6,3 +6,34 @@ function feetToMile(feet){
 }
 var result = feetToMile(1);
 console.log(result);
+
+//Make Wood Calculator
+
+function woodCalculator([chair,table,bed]){
+    var result = (chair * 1) + (table * 3) + (bed * 5);
+    console.log(result);
+}
+woodCalculator([14,5,12]);
+
+// Make a brick calculator
+
+function brickCalculator(n){
+    if( n <= 10){
+        var result = 1000 * 15;
+        console.log(result);
+    }
+    else if( n > 10 , n <= 20 ){
+        var result = 1000 * 12;
+        console.log(result);
+    }
+    else if (n > 20) {
+        var result = 1000 * 10;
+        console.log(result);
+    }
+    else{
+        var result = "Your input is wrong";
+        console.log(result);
+    }
+
+}
+brickCalculator(19);
